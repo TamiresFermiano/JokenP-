e4a927364c748144981a0172f79da6f640a49ffc
Tamires Fermiano <tamiresfgirardi@gmail.com>
2023-07-17 21:46:31 -0300

    meu primeiro commit

---
+++ scripts.js
@@ -0,0 +1,48 @@
+
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+
+
+
+const playHuman = (humanChoice) => {
+
+  playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+  const choices = ['rock', 'paper', 'scissors']
+  const randomNumber = Math.floor(Math.random() * 3)
+
+
+  return choices[randomNumber]
+}
+const playTheGame = (human, machine) => {
+  console.log('humano:' + human + "maquina" + machine)
+
+  if (human === machine) {
+    result.innerHTML = "Deu empate"
+  } else if (
+    (human === 'paper' && machine === 'rock') ||
+    (human === 'rock' && machine === 'scissor') ||
+    (human === 'scissor' && machine === 'paper')
+  ) {
+    humanScoreNumber++
+    humanScore.innerHTML = humanScoreNumber
+    result.innerHTML = "Você ganhou"
+    
+  } else {
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber
+    result.innerHTML = "Você perdeu"
+  }
+
+
+
+
+}
+
